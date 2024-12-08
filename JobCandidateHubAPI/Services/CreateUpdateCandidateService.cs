c14b9220577edf34bae5f23b7d2bc3c26e9712d0
sushilrai01 <ssilray16@gmail.com>
2024-12-08 22:28:36 +0545

    create/update candidate if found

--- JobCandidateHubAPI/Services/CreateUpdateCandidateService.cs
+++ JobCandidateHubAPI/Services/CreateUpdateCandidateService.cs
@@ -16,23 +16,44 @@ namespace JobCandidateHubAPI.Services
         {
             await using var context = new JobCandidateHubDbContext(_dbOptions.ConOptions);
 
-            var candidate = new TblCandidate()
-            {
-                Id = Guid.NewGuid(),
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                EmailAddress =model.EmailAddress,
-                CallTimeInterval = model.CallTimeInterval,
-                Comment =model.Comment,
-                GithubUrl = model.GithubUrl,
-                LinkedInUrl =model.LinkedInUrl,
-                PhoneNo = model.PhoneNo,
-            };
+            var newGuid = Guid.NewGuid();
 
-            context.TblCandidates.Add(candidate);
+            var candidates =  await context.TblCandidates.Where(x =>
+                x.EmailAddress.Trim().ToLower() == model.EmailAddress.Trim().ToLower()).FirstOrDefaultAsync();
+
+            if (candidates != null)
+            {
+                newGuid = candidates.Id;
+                candidates.FirstName = model.FirstName;
+                candidates.LastName = model.LastName;
+                candidates.EmailAddress = model.EmailAddress;
+                candidates.Comment = model.Comment;
+                candidates.CallTimeInterval = model.CallTimeInterval;
+                candidates.GithubUrl = model.GithubUrl;
+                candidates.LinkedInUrl = model.LinkedInUrl;
+                candidates.PhoneNo = model.PhoneNo;
+            }
+            else
+            {
+                var candidate = new TblCandidate()
+                {
+                    Id = newGuid,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    CallTimeInterval = model.CallTimeInterval,
+                    Comment = model.Comment,
+                    GithubUrl = model.GithubUrl,
+                    LinkedInUrl = model.LinkedInUrl,
+                    PhoneNo = model.PhoneNo,
+                };
+
+                context.TblCandidates.Add(candidate);
+            }
+
             await context.SaveChangesAsync();
 
-            return candidate.Id;
+            return newGuid;
         }
     }
 }
